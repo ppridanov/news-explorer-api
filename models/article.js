2b4329cc9016c37042337e019b2f35333d9d8775
unknown <Petr Pridanov>
2020-02-06 09:56:32 +0600

    Create all needs files

---
+++ models/article.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^https?:\/\//.test(v);
+      },
+      message: (props) => `${props.value} это не правильная ссылка!`,
+    }
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /^https?:\/\//.test(v);
+      },
+      message: (props) => `${props.value} это не правильная ссылка!`,
+    }
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false
+  }
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('article', userSchema);
